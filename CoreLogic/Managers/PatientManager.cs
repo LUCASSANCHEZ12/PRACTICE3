157e22d4794ce7eec86a3a3dfd2b61869b42ddeb
LUCASSANCHEZ12 <lucassanchez1@upb.edu>
2023-05-04 16:00:48 -0400

    P3-001, Created 2 new directories Managers and Models, with 2 new classes Patients.cs and PatientManager.cs

---
+++ CoreLogic/Managers/PatientManager.cs
@@ -0,0 +1,14 @@
+using UPB.CoreLogic.Models;
+using System;
+
+namespace UPB.CoreLogic.Managers;
+
+public class PatientManager
+{
+    private string _path;
+    
+    public PatientManager(string path)
+    {
+        _path = path;
+    }  
+}
